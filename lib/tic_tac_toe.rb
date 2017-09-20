870fcad32ae041bcc51f120ee7a1d6ac75cfd3bb
Sophie Rae <sophie.rae.06@gmail.com>
2017-09-20 12:30:12 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,5 +47,94 @@ WIN_COMBINATIONS = [
   end
   end
 
-  
+  def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board,index)
+    move(board,index,current_player(board))
+  else
+    turn(board)
+  end
+  display_board(board)
+end
+
+def turn_count(board)
+  turns_taken = 0
+  board.each do |occupied_position|
+    if occupied_position == "X" || occupied_position == "O"
+      turns_taken +=1
+    end
+  end
+  turns_taken
+end
+
+def current_player(board)
+  if turn_count(board).even?
+    return "X"
+  else
+    return "O"
+  end
+end
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combination
+    end
+  end
+  false
+end
+
+def full?(board)
+  space = " "
+  if board.include?(space)
+    return false
+  else
+    return true
+  end
+end
+
+def draw?(board)
+if won?(board) == false && full?(board) == true
+  return true
+else
+  return false
+end
+end
+
+def over?(board)
+if won?(board) != false || draw?(board) == true || full?(board) == true
+  return true
+else
+  return false
+end
+end
+
+def winner(board)
+  if win = won?(board)
+    board[win.first]
+    end
+  end
+
+def play(board)
+until over?(board)
+  turn(board)
+end
+if won?(board)
+  puts "Congratulations #{winner(board)}!"
+else draw?(board)
+  puts "Cat's Game!"
+end
+
 end
