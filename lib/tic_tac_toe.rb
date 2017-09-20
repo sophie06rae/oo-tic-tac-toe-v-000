b65b5f68f9309d917c0276a002c92c42d9de5ff9
Sophie Rae <sophie.rae.06@gmail.com>
2017-09-20 12:25:35 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,7 +51,7 @@ WIN_COMBINATIONS = [
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  valid_move?(index) ? move(index) : turn
+  valid_move?(index) ? move(index, current_player) : turn
   display_board
   end
 
